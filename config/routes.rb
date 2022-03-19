35fc78fb6f2e5596a941162c9428ef0eb0cb85a9
asinaga117 <100944572+asinaga117@users.noreply.github.com>
2022-03-19 13:54:54 +0000

    top.html.erbの記載

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'books/top' => "books#top"
   get 'homes/top' => "homes#top"
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
