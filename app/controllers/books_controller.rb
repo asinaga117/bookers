3cc3434bbb7c17f848ed281abd864907ff5e9e90
asinaga117 <100944572+asinaga117@users.noreply.github.com>
2022-03-20 13:47:15 +0000

    bookの新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,27 @@
 class BooksController < ApplicationController
+  
   def top
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new(book_params)
+    
+    book.save
+    
+    redirect_to "/top"
+  end
+  
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'books/top' => "books#top"
-  get 'homes/top' => "homes#top"
+  get 'books/top'
+  get 'books/show'
+  get 'books/edit'
+  get 'books/destroy'
+  get '/top' => "homes#top"
+  resources :books
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
